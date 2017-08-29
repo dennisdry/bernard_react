dfcc3e545a6f5d8d3a1aab65184e6c97212294e0
Zsolt Nagy <zsoltnagy@Zsolt-MacBook-Pro.local>
2017-08-29 10:08:51 +0200

    fixed message

--- dev/src/components/Header/header-profile.js
+++ dev/src/components/Header/header-profile.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import ProfilePicture from './../Profile/profile-picture';
-import NotificationMessage from './../Notifications/notification-message';
+//import NotificationMessage from './../Notifications/notification-message';
 
 export default class HeaderProfile extends React.Component {
-constructor(props) {
-  super(props)
-  this.state = {
-    notifications: this.props.numberOfNotifications
-  }
-
-  this.numberOfNotifications = this.numberOfNotifications.bind(this);
-
-}
-
-numberOfNotifications() {
-  var notificationNumber = this.props.numberOfNotifications
-
-  if (notificationNumber > 0) {
-    return (
-      <NotificationMessage />
-    );
-  }
-
-  console.log(notificationNumber);
-}
+// constructor(props) {
+//   super(props)
+//   this.state = {
+//     notifications: this.props.numberOfNotifications
+//   }
+//
+//   this.numberOfNotifications = this.numberOfNotifications.bind(this);
+//
+// }
+//
+// numberOfNotifications() {
+//   var notificationNumber = this.props.numberOfNotifications
+//
+//   if (notificationNumber > 0) {
+//     return (
+//       <NotificationMessage />
+//     );
+//   }
+//
+//   console.log(notificationNumber);
+// }
 
   render() {
     return (
       <div className="col col-lg-2 hidden-md-down user-profile-wrapper">
         <div className="user-profile">
           <ProfilePicture name="Harry Smith" size={65}  />
-          {this.numberOfNotifications()}
         </div>
       </div>
     );
